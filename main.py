c0504ab46ef0d9f21f2b766ef647c363b6bd0f89
Alaa Saleem <127680801+alaasaleem@users.noreply.github.com>
2023-05-27 12:46:47 +0300

    🚀 Added Flask login and registration functionality 🌐
    This commit adds a Flask application with login and registration pages. Users can now create an account, log in with their credentials, and be redirected to a success page upon successful login or registration.
    
    The login page validates the user's email and password against the MySQL database, while the registration page checks for existing users before inserting a new user record.
    
    The application is connected to a MySQL database using the provided configuration parameters.

---
+++ main.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+
+app = Flask(__name__)
+
+# Configure your MySQL database connection
+db = mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="DBproject2023!",
+    database="bookingsystem"
+)
+
+
+# Define a route for the login page
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        # Validate the login credentials in your database
+        cursor = db.cursor()
+        query = "SELECT * FROM user WHERE email = %s AND password = %s"
+        cursor.execute(query, (email, password))
+        user = cursor.fetchone()
+
+        if user:
+            # Redirect to a success page or perform further actions
+            return redirect('/success')
+        else:
+            # Display an error message on the login page
+            error = 'Invalid email or password. Please try again.'
+            return render_template('login.html', error=error)
+    else:
+        return render_template('login.html')
+
+
+# Define a route for the registration page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        # Check if the user already exists in the database
+        cursor = db.cursor()
+        query = "SELECT * FROM user WHERE email = %s"
+        cursor.execute(query, (email,))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            error = 'User with this email already exists. Please try a different email.'
+            return render_template('register.html', error=error)
+        else:
+            # Insert the new user into the database
+            insert_query = "INSERT INTO user (email, password) VALUES (%s, %s)"
+            cursor.execute(insert_query, (email, password))
+            db.commit()
+
+            # Redirect to a success page or perform further actions
+            return redirect('/success')
+    else:
+        return render_template('register.html')
+
+
+# Define a route for the success page
+@app.route('/success')
+def success():
+    return "Login or registration successful!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
