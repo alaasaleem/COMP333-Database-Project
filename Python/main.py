9fff6a75ce93e89332a2e2f8219b94b7b3cb020a
Alaa Saleem <127680801+alaasaleem@users.noreply.github.com>
2023-06-26 20:01:19 +0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,99 +1,31 @@
-from flask import Flask, render_template, request, redirect
-from login_register import app, validate_login, register_user
-from delete_operator import find_operator, delete_operator
+from flask import Flask
+import mysql.connector
 
+app = Flask(__name__)
 
-# Define a route for the login page
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+# Configure your MySQL database connection
+db = mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="DBproject2023!",
+    database="bookingsystem"
+)
 
-        role = validate_login(email, password)
+def add_operator(email, password, first_name, last_name):
+    cursor = db.cursor()
 
-        if role == 'user':
-            return redirect('/user')
-        elif role == 'operator':
-            return redirect('/operator')
-        elif role == 'admin':
-            return redirect('/admin')
-        else:
-            error = 'Invalid email or password. Please try again.'
-            return render_template('login.html', error=error)
+    # Check if the email already exists in the operator table
+    query = "SELECT * FROM operator WHERE email = %s"
+    cursor.execute(query, (email,))
+    existing_operator = cursor.fetchone()
 
-    else:
-        error = None
+    if existing_operator:
+        return 'Operator with this email already exists. Please try a different email.'
 
-    # Render the login page
-    return render_template('login.html', error=error)
+    # Insert the new operator into the operator table
+    insert_query = "INSERT INTO operator (email, password, first_name, last_name) VALUES (%s, %s, %s, %s)"
+    cursor.execute(insert_query, (email, password, first_name, last_name))
+    db.commit()
 
-# Define a route for the user page
-@app.route('/user')
-def user():
-    return render_template('user.html')
-
-# Define a route for the operator page
-@app.route('/operator')
-def operator():
-    return render_template('operator.html')
-
-# Define a route for the admin page
-@app.route('/admin')
-def admin():
-    return render_template('admin.html')
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-
-        error = register_user(email, password, first_name, last_name)
-
-        if error:
-            return render_template('register.html', error=error)
-
-        # Redirect to a success page or perform further actions
-        return redirect('/success')
-
-    else:
-        return render_template('register.html')
-
-
-# Define a route for the operator page
-@app.route('/manage_operator')
-def manage_operator():
-    return render_template('manage_operator.html')
-
-# Define a route for the reports page
-@app.route('/manage_reports')
-def manage_reports():
-    return render_template('manage_reports.html')
-
-@app.route('/delete_operator', methods=['GET', 'POST'])
-def delete_operator_route():
-    if request.method == 'POST':
-        search_query = request.form['search_query']
-        operator = find_operator(search_query)
-
-        if operator:
-            return render_template('delete_operator.html', operators=operator)
-        else:
-            error = 'Operator not found in the database or operator table.'
-            return render_template('delete_operator.html', error=error)
-
-    else:
-        return render_template('delete_operator.html')
-
-
-@app.route('/delete_operator_confirm', methods=['POST'])
-def delete_operator_confirm():
-    operator_id = request.form['operator_id']
-    delete_operator(operator_id)
-    return redirect('/manage_operator')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return None
